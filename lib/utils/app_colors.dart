035455f6906ae958d1dd2c22cec45f4bf612544d
Fumino The Fish <richberchannel01@gmail.com>
2025-06-18 22:23:04 +0700

    feat: add AppColors class with comprehensive color definitions for the application

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,100 @@
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names
+
+import 'package:medlink/utils/app_imports.dart';
+
+class AppColors {
+  // primary color
+  static const Color primary900 = Color(0xFF821A25);
+  static const Color primary800 = Color(0xFF9D1726);
+  static const Color primary700 = Color(0xFFBE1729);
+  static const Color primary600 = Color(0xFFDF1D32);
+  static const Color primary500 = Color(0xFFF53E52);
+  static const Color primary400 = Color(0xFFFC6D7C);
+  static const Color primary300 = Color(0xFFFFA2AC);
+  static const Color primary200 = Color(0xFFFFC8CE);
+  static const Color primary100 = Color(0xFFFFE1E4);
+  static const Color primary50 = Color(0xFFFEF2F3);
+
+  //Neutral color
+  static const Color primaryText = Color(0xFF111111);
+  static const Color secondaryText = Color(0xFF646464);
+  static const Color disable = Color(0xFFA7A7A7);
+  static const Color border = Color(0xFFDEDEDE);
+  static const Color dividers = Color(0xFFEFEFEF);
+  static const Color background = Color(0xFFF6F6F6);
+  static const Color white = Color(0xFFFFFFFF);
+
+  //Functional color
+  static const Color errorMain = Color(0xFFE82C36);
+  static const Color errorLight = Color(0xFFFFF1F2);
+  static const Color infoMain = Color(0xFF1371DC);
+  static const Color infoLight = Color(0xFFEEF9FF);
+  static const Color successMain = Color(0xFF54B12A);
+  static const Color successLight = Color(0xFFF1FBEA);
+
+  //Other color
+  static const Color otherColor1 = Color(0xFFFEF5E9);
+  static const Color otherColor2 = Color(0xFFF09D2E);
+  static const Color otherColor3 = Color(0xFFE48729);
+
+  /// appbar color
+  static const Color color1 = Color(0xFF02AFB7);
+  static const Color color2 = Color(0xFFF6F6F6);
+  static const Color color3 = Color(0xFFF6F6F6);
+  static const Color color4 = Color(0xFF646464);
+  static const Color color5 = Color(0xFFEFEFEF);
+
+  static const Color checkColor1 = Color(0xffFF835B);
+  static const Color checkColor2 = Color(0xffD4E183);
+
+  static const Color chatColor1 = Colors.blue;
+
+  static const Color WHITE = Colors.white;
+  static const Color BLACK = Colors.black;
+  static const Color BLACK38 = Colors.black38;
+  static const Color BLACK26 = Colors.black26;
+  static const Color BLACK45 = Colors.black45;
+  static const Color GREEN = Colors.green;
+  static const Color YELLOW = Colors.yellow;
+  static Color LIGHT_GREY_SCREEN_BACKGROUND = Colors.grey.shade200;
+  static Color LIGHT_GREY_TEXT = Colors.grey.shade500;
+  static Color GREY = Colors.grey.shade500;
+  static Color LIGHT_BLACK_TEXT = Colors.black54;
+  static Color LIGHT_BLUE_ACCENT = Colors.lightBlueAccent;
+  static Color GREEN_ACCENT = Colors.greenAccent;
+  static Color BLUE_GREY = Colors.blueGrey;
+  static Color BLUE_ACCENT = Colors.blueAccent;
+
+  static Color AMBER = Colors.amber.shade700;
+  static Color AMBER_NORMAL = Colors.amber;
+  static Color RED = Colors.red;
+  static Color RED800 = Colors.red.shade800;
+  static Color RED700 = Colors.red.shade700;
+
+  static Color RedErr = const Color(0xFFE82C36);
+
+  static Color iconColor = const Color(0xffDDDDDD);
+  static Color chooseColor = const Color(0xff707070);
+
+  static Color choosePlanColor = const Color(0xff030303);
+  static Color transparentColor = Colors.transparent;
+  static Color themeColor1 = Colors.cyanAccent.shade700;
+  static Color themeColor2 = Colors.cyanAccent;
+  static Color themeColor3 = Colors.grey.shade700;
+  static Color greyShade3 = Colors.grey.shade300;
+  static Color greyShade4 = Colors.grey.shade400;
+  static Color greyShade6 = Colors.grey.shade600;
+  static Color greyShade1 = Colors.grey.shade100;
+  static Color grey = Colors.grey;
+
+  static Color appointmentDetailsReportBgColor = const Color(0xFFFFB146);
+  static Color noPrescriptionTextColor = const Color(0xFF7B7B7B);
+  static Color reportTextColor = const Color(0xFF323232);
+  static Color chatUnSendBarrierColor = Colors.black87.withOpacity(0.7);
+  static Color dSubscriptionPriceBg = const Color(0xffEEEEEE).withOpacity(0.9);
+  static Color tileColorIncomeReport = const Color(0xFF1e1e1e);
+  static Color tileBgColorIncomeReport = const Color(0xFFeeeeee);
+  static Color incomeReportBgColor = const Color(0xFFe3fffd);
+  static Color totalIncomeTextColor = const Color(0xFF757575);
+  static Color totalFilterReportTextColor = const Color(0xFF808080);
+}
