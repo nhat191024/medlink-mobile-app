ea609d3c370e63320c9c35c23d6681fb59ec65bb
Fumino The Fish <richberchannel01@gmail.com>
2025-06-18 22:23:22 +0700

    feat: add common_imports.dart for centralized export of splash screen components

---
+++ lib/common/utils/common_imports.dart
@@ -0,0 +1,3 @@
+export 'package:medlink/common/controllers/splash_controller.dart';
+export 'package:medlink/common/bindings/splash_screen_binding.dart';
+export 'package:medlink/common/screens/splash_screen.dart';
