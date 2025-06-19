3868f0bda4892c859959d0ebbdd23d8db6a597d1
Fumino The Fish <richberchannel01@gmail.com>
2025-06-19 14:54:14 +0700

    feat: reorganize common imports for splash and login screens

--- lib/common/utils/common_imports.dart
+++ lib/common/utils/common_imports.dart
@@ -1,3 +1,9 @@
+//splash
 export 'package:medlink/common/controllers/splash_controller.dart';
 export 'package:medlink/common/bindings/splash_screen_binding.dart';
 export 'package:medlink/common/screens/splash_screen.dart';
+
+//login
+export 'package:medlink/common/controllers/login_controller.dart';
+export 'package:medlink/common/bindings/login_screen_binding.dart';
+export 'package:medlink/common/screens/login_screen.dart';
