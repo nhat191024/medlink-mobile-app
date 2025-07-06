0e5523907ae4a9947c3333f78c1fdbb143bc375b
Fumino The Fish <richberchannel01@gmail.com>
2025-07-06 23:20:16 +0700

    feat: add patient imports for home screen functionality

---
+++ lib/patient/utils/patient_imports.dart
@@ -0,0 +1,6 @@
+//home screen
+export 'package:medlink/patient/controllers/bottom_nav_controller.dart';
+export 'package:medlink/patient/controllers/home_controller.dart';
+export 'package:medlink/patient/screens/patient_bottom_nav.dart';
+export 'package:medlink/patient/screens/home_screen.dart';
+export 'package:medlink/patient/bindings/bottom_tab_binding.dart';
