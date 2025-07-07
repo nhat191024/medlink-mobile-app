97b687ef40921b1b9e95691a4a580b30dd509a8a
Fumino The Fish <richberchannel01@gmail.com>
2025-07-07 17:00:19 +0700

    feat: add ServiceModel class for managing service data serialization

---
+++ lib/model/service_model.dart
@@ -0,0 +1,39 @@
+class ServiceModel {
+  int? id;
+  String? icon;
+  String? name;
+  String? description;
+  int? price;
+  int? duration;
+  int? bufferTime;
+  String? seat;
+  bool? isActive;
+
+  ServiceModel({this.name, this.icon});
+
+  ServiceModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    icon = json['icon'];
+    name = json['name'];
+    description = json['description'];
+    price = json['price'];
+    duration = json['duration'];
+    bufferTime = json['buffer_time'];
+    seat = json['seat'];
+    isActive = json['is_active'] == 'false' ? false : true;
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'icon': icon,
+      'name': name,
+      'description': description,
+      'price': price,
+      'duration': duration,
+      'buffer_time': bufferTime,
+      'seat': seat,
+      'is_active': isActive,
+    };
+  }
+}
