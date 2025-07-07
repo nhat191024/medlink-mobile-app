1ef8e8753b61126f64b3b15e756f31993c9b20cc
Fumino The Fish <richberchannel01@gmail.com>
2025-07-07 17:04:31 +0700

    feat: update patient_imports to include search and booking controllers and bindings

--- lib/patient/utils/patient_imports.dart
+++ lib/patient/utils/patient_imports.dart
@@ -4,3 +4,11 @@ export 'package:medlink/patient/controllers/home_controller.dart';
 export 'package:medlink/patient/screens/patient_bottom_nav.dart';
 export 'package:medlink/patient/screens/home_screen.dart';
 export 'package:medlink/patient/bindings/bottom_tab_binding.dart';
+
+//search screen
+export 'package:medlink/patient/controllers/search_controller.dart';
+export 'package:medlink/patient/screens/search_category_screen.dart';
+export 'package:medlink/patient/bindings/search_doctor_binding.dart';
+
+//booking screen
+export 'package:medlink/patient/controllers/booking_controller.dart';
