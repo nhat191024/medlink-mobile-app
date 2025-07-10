de84bb255c956c4b536e37200666bd9382315604
Fumino The Fish <richberchannel01@gmail.com>
2025-07-10 23:07:57 +0700

    fix: update app environment from 'local' to 'host' for API configuration

--- lib/utils/app_apis.dart
+++ lib/utils/app_apis.dart
@@ -1,6 +1,6 @@
 class Apis {
   static const int timeOut = 10;
-  static const appEnv = 'local';
+  static const appEnv = 'host';
   static const String serverAddress = 'http://113.186.124.48:33003';
   static const String api = appEnv == 'local'
       ? 'http://192.168.1.4:8000/api/'
