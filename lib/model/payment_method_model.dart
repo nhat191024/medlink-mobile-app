961df3030ce6b31efd033433a4cf16cc0757033f
Fumino The Fish <richberchannel01@gmail.com>
2025-07-10 13:17:58 +0700

    feat: implement PaymentMethodModel class with JSON serialization

---
+++ lib/model/payment_method_model.dart
@@ -0,0 +1,32 @@
+class PaymentMethodModel {
+  String name;
+  String id;
+  String icon;
+  String info;
+  String provider;
+  String? expiry;
+  bool isCard;
+
+  PaymentMethodModel({
+    required this.name,
+    required this.id,
+    required this.icon,
+    required this.info,
+    required this.provider,
+    this.expiry,
+    this.isCard = false,
+  });
+
+  PaymentMethodModel.fromJson(Map<String, dynamic> json)
+    : name = json['name'],
+      id = json['id'],
+      icon = json['icon'],
+      info = json['info'],
+      provider = json['provider'],
+      expiry = json['expiry'],
+      isCard = json['is_Card'] == 'true' ? true : false;
+
+  Map<String, dynamic> toJson() {
+    return {'name': name, 'icon': icon, 'info': info, 'provider': provider, 'expiry': expiry};
+  }
+}
