202bb0bed8839cdb1d9f21c1ed77411ee4f84dcf
Fumino The Fish <richberchannel01@gmail.com>
2025-07-11 19:08:41 +0700

    feat: add bottom navigation bar exports for doctor module

---
+++ lib/doctor/utils/doctor_imports.dart
@@ -0,0 +1,4 @@
+//botom nav bar
+export 'package:medlink/doctor/controllers/bottom_nav_controller.dart';
+export 'package:medlink/doctor/screens/doctor_bottom_nav.dart';
+export 'package:medlink/doctor/bindings/bottom_nav_binding.dart';
