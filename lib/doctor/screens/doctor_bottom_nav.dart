1d0de582caf68a506703f089a82b6db9d0c2ea8f
Fumino The Fish <richberchannel01@gmail.com>
2025-07-11 19:09:07 +0700

    feat: add DoctorBottomNav component with bottom navigation bar

---
+++ lib/doctor/screens/doctor_bottom_nav.dart
@@ -0,0 +1,63 @@
+import 'package:medlink/utils/app_imports.dart';
+import 'package:medlink/doctor/controllers/bottom_nav_controller.dart';
+
+class DoctorBottomNav extends GetView<DoctorBottomNavController> {
+  const DoctorBottomNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Obx(() => controller.currentScreen),
+      bottomNavigationBar: Obx(() {
+        return SizedBox(
+          height: 75,
+          child: BottomNavigationBar(
+            currentIndex: controller.selectedIndex.value,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  controller.selectedIndex.value == 0 ? AppImages.homeSelect : AppImages.home,
+                ),
+                label: 'Dashboard',
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  controller.selectedIndex.value == 1 ? AppImages.messageSelect : AppImages.messageTyping,
+                ),
+                label: 'Messages',
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  controller.selectedIndex.value == 2 ? AppImages.calenderSelect : AppImages.calenderCheck,
+                ),
+                label: 'Appointment',
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  controller.selectedIndex.value == 3 ? AppImages.gearSixSelect : AppImages.gearSix,
+                ),
+                label: 'Settings',
+              ),
+            ],
+            selectedItemColor: AppColors.primary600,
+            unselectedItemColor: AppColors.disable,
+            selectedLabelStyle: const TextStyle(
+              fontFamily: AppFontStyleTextStrings.regular,
+              fontSize: 12,
+              color: AppColors.primary600,
+            ),
+            unselectedLabelStyle: const TextStyle(
+              fontFamily: AppFontStyleTextStrings.regular,
+              fontSize: 12,
+              color: AppColors.disable,
+            ),
+            onTap: (index) {
+              controller.changeIndex(index);
+            },
+          ),
+        );
+      }),
+    );
+  }
+}
