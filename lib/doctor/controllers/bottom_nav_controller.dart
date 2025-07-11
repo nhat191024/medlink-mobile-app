be2a7c691d4b0e1cc0e9cfa38f46f6df0251bc4d
Fumino The Fish <richberchannel01@gmail.com>
2025-07-11 23:43:38 +0700

    feat: update DoctorBottomNavController to include MessagesScreen and MyAppointmentsScreen

--- lib/doctor/controllers/bottom_nav_controller.dart
+++ lib/doctor/controllers/bottom_nav_controller.dart
@@ -3,14 +3,17 @@ import 'package:medlink/utils/app_imports.dart';
 import 'package:medlink/doctor/screens/home_screen.dart';
 import 'package:medlink/doctor/controllers/home_controller.dart';
 
+import 'package:medlink/doctor/screens/my_appointments_screen.dart';
+import 'package:medlink/common/screens/messages/messages_screen.dart';
+
 class DoctorBottomNavController extends GetxController {
   final DoctorHomeController controllers = Get.put(DoctorHomeController());
   var selectedIndex = 0.obs;
 
   final List<Widget> screens = [
     DoctorHomeScreen(),
-    // MessagesScreen(),
-    // DoctorMyAppointmentsScreen(),
+    MessagesScreen(),
+    DoctorMyAppointmentsScreen(),
     // SettingScreen(),
   ];
 
