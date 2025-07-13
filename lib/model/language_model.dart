eff9634d9957deefb6b27c6d2ca1312f34b77367
Fumino The Fish <richberchannel01@gmail.com>
2025-07-14 01:49:40 +0700

    fix: update LanguageModel to use non-nullable fields and improve constructor initialization

--- lib/model/language_model.dart
+++ lib/model/language_model.dart
@@ -1,13 +1,10 @@
 class LanguageModel {
-  String? code;
-  String? name;
+  String code;
+  String name;
 
-  LanguageModel({this.code, this.name});
+  LanguageModel({required this.code, required this.name});
 
-  LanguageModel.fromJson(Map<String, dynamic> json) {
-    code = json['code'];
-    name = json['name'];
-  }
+  LanguageModel.fromJson(Map<String, dynamic> json) : code = json['code'], name = json['name'];
 
   Map<String, dynamic> toJson() {
     return {'code': code, 'name': name};
