8e9ab11cbf434a4f887961eebf3cf7a5ef0cbb7d
Fumino The Fish <richberchannel01@gmail.com>
2025-07-14 01:49:29 +0700

    fix: update TopReviewModel to use non-nullable fields and improve constructor initialization

--- lib/model/top_review_model.dart
+++ lib/model/top_review_model.dart
@@ -1,21 +1,27 @@
 class TopReviewModel {
-  int? id;
-  double? rate;
-  String? review;
-  String? name;
-  String? avatar;
-  String? createdAt;
+  int id;
+  double rate;
+  String review;
+  String name;
+  String avatar;
+  String createdAt;
 
-  TopReviewModel({this.id, this.rate, this.review, this.name, this.avatar, this.createdAt});
+  TopReviewModel({
+    required this.id,
+    required this.rate,
+    required this.review,
+    required this.name,
+    required this.avatar,
+    required this.createdAt,
+  });
 
-  TopReviewModel.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    rate = json['rate'] != null ? (json['rate'] as num).toDouble() : 0.0;
-    review = json['review'];
-    name = json['name'];
-    avatar = json['avatar'];
-    createdAt = json['created_at'];
-  }
+  TopReviewModel.fromJson(Map<String, dynamic> json)
+    : id = json['id'],
+      rate = json['rate'] != null ? (json['rate'] as num).toDouble() : 0.0,
+      review = json['review'],
+      name = json['name'],
+      avatar = json['avatar'],
+      createdAt = json['created_at'];
 
   Map<String, dynamic> toJson() {
     return {
