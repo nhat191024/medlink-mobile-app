0724e03ab925740372c5e53b97dd6611090217bc
Fumino The Fish <richberchannel01@gmail.com>
2025-07-14 00:26:03 +0700

    refactor: organize imports in doctor_imports.dart for better clarity

--- lib/doctor/utils/doctor_imports.dart
+++ lib/doctor/utils/doctor_imports.dart
@@ -3,7 +3,12 @@ export 'package:medlink/doctor/controllers/bottom_nav_controller.dart';
 export 'package:medlink/doctor/screens/doctor_bottom_nav.dart';
 export 'package:medlink/doctor/bindings/bottom_nav_binding.dart';
 
-// settings screen
+//service settings
 export 'package:medlink/doctor/controllers/settings/service_controller.dart';
 export 'package:medlink/doctor/screens/settings/my_services_screen.dart';
 export 'package:medlink/doctor/bindings/service_setting_binding.dart';
+
+//work schedules
+export 'package:medlink/doctor/controllers/settings/work_schedules_controller.dart';
+export 'package:medlink/doctor/screens/settings/work_schedules_screen.dart';
+export 'package:medlink/doctor/bindings/working_schedule_binding.dart';
