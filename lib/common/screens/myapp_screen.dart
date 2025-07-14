e5a66c482280f961531dae6b0c78e01ff52f19db
Fumino The Fish <richberchannel01@gmail.com>
2025-07-14 17:04:28 +0700

    feat: update localization settings and clean up theme text styles in MyApp

--- lib/common/screens/myapp_screen.dart
+++ lib/common/screens/myapp_screen.dart
@@ -15,19 +15,15 @@ class MyApp extends GetView<SplashController> {
       initialRoute: AppPages.initialRoute,
       getPages: AppPages.routes,
       defaultTransition: Transition.cupertino,
-      transitionDuration: const Duration(
-        milliseconds: 500,
-      ),
+      transitionDuration: const Duration(milliseconds: 500),
       translations: Words(),
-      locale: const Locale('en', 'US'),
+      locale: const Locale('vi', 'VN'),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: false,
         timePickerTheme: TimePickerThemeData(
           dayPeriodTextColor: AppColors.themeColor1,
-          helpTextStyle: const TextStyle(
-            fontFamily: AppFontStyleTextStrings.regular,
-          ),
+          helpTextStyle: const TextStyle(fontFamily: AppFontStyleTextStrings.regular),
         ),
         hintColor: AppColors.themeColor1,
         primaryColor: AppColors.themeColor2,
@@ -35,55 +31,26 @@ class MyApp extends GetView<SplashController> {
         primaryColorDark: AppColors.themeColor3,
         primaryColorLight: AppColors.LIGHT_GREY_SCREEN_BACKGROUND,
         textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            fontFamily: AppFontStyleTextStrings.regular,
-          ),
-          displayMedium: TextStyle(
-            fontFamily: AppFontStyleTextStrings.regular,
-          ),
-          displaySmall: TextStyle(
-            fontFamily: AppFontStyleTextStrings.regular,
-          ),
-          headlineMedium: TextStyle(
-            fontFamily: AppFontStyleTextStrings.regular,
-          ),
-          headlineSmall: TextStyle(
-            fontFamily: AppFontStyleTextStrings.regular,
-          ),
-          titleLarge: TextStyle(
-            fontFamily: AppFontStyleTextStrings.medium,
-          ),
-          titleMedium: TextStyle(
-            fontFamily: AppFontStyleTextStrings.regular,
-          ),
-          titleSmall: TextStyle(
-            fontFamily: AppFontStyleTextStrings.medium,
-          ),
-          bodySmall: TextStyle(
-            fontSize: 10,
-            fontFamily: AppFontStyleTextStrings.regular,
-          ),
-          bodyLarge: TextStyle(
-            fontSize: 13,
-            fontFamily: AppFontStyleTextStrings.medium,
-          ),
-          bodyMedium: TextStyle(
-            fontSize: 13,
-            fontFamily: AppFontStyleTextStrings.light,
-          ),
-          labelLarge: TextStyle(
-            fontFamily: AppFontStyleTextStrings.medium,
-          ),
+          displayLarge: TextStyle(fontFamily: AppFontStyleTextStrings.regular),
+          displayMedium: TextStyle(fontFamily: AppFontStyleTextStrings.regular),
+          displaySmall: TextStyle(fontFamily: AppFontStyleTextStrings.regular),
+          headlineMedium: TextStyle(fontFamily: AppFontStyleTextStrings.regular),
+          headlineSmall: TextStyle(fontFamily: AppFontStyleTextStrings.regular),
+          titleLarge: TextStyle(fontFamily: AppFontStyleTextStrings.medium),
+          titleMedium: TextStyle(fontFamily: AppFontStyleTextStrings.regular),
+          titleSmall: TextStyle(fontFamily: AppFontStyleTextStrings.medium),
+          bodySmall: TextStyle(fontSize: 10, fontFamily: AppFontStyleTextStrings.regular),
+          bodyLarge: TextStyle(fontSize: 13, fontFamily: AppFontStyleTextStrings.medium),
+          bodyMedium: TextStyle(fontSize: 13, fontFamily: AppFontStyleTextStrings.light),
+          labelLarge: TextStyle(fontFamily: AppFontStyleTextStrings.medium),
         ),
       ),
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: const [
-        Locale('en', 'US'),
-        Locale('fr', ''),
-      ],
+      supportedLocales: const [Locale('en', 'US'), Locale('vi', 'VN')],
     );
   }
 }
