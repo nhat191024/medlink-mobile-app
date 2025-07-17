944b2c88b7eb52fc32065a09467301c5046adada
Fumino The Fish <richberchannel01@gmail.com>
2025-07-17 22:44:39 +0700

    fix: change constructor to non-const and ensure controller initialization in SearchCategoryScreen

--- lib/patient/screens/search_category_screen.dart
+++ lib/patient/screens/search_category_screen.dart
@@ -4,7 +4,9 @@ import 'package:medlink/patient/utils/patient_imports.dart';
 import 'package:medlink/components/widget/grid_category.dart';
 
 class SearchCategoryScreen extends GetView<SearchHeathCareController> {
-  const SearchCategoryScreen({super.key});
+  SearchCategoryScreen({super.key});
+
+  final SearchHeathCareController controllers = Get.put(SearchHeathCareController());
 
   static const double _headerPaddingTop = 40.0;
   static const double _headerPaddingHorizontal = 20.0;
