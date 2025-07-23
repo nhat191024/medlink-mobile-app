9475ce691d29f21d519984ef381ad8d2fcdf64f7
Fumino The Fish <richberchannel01@gmail.com>
2025-07-23 23:26:11 +0700

    fix: update API server address for correct endpoint

--- lib/utils/app_apis.dart
+++ lib/utils/app_apis.dart
@@ -3,6 +3,6 @@ class Apis {
   static const appEnv = 'host';
   static const String serverAddress = 'http://113.186.124.48:33003';
   static const String api = appEnv == 'local'
-      ? 'http://192.168.1.4:8000/api/'
+      ? 'http://192.168.1.6:10002/api/'
       : 'https://medlink.taiyo.space/api/';
 }
