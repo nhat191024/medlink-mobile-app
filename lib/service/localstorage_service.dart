2fcc30e412e6447f9a3aefbb91b90d0fd2e0eebb
Fumino The Fish <richberchannel01@gmail.com>
2025-07-25 19:57:08 +0700

    refactor: simplify method signatures in StorageService for improved readability

--- lib/service/localstorage_service.dart
+++ lib/service/localstorage_service.dart
@@ -1,38 +1,30 @@
-
 import 'package:get_storage/get_storage.dart' hide Data;
+
 final box = GetStorage();
 
 class StorageService {
-  static dynamic readData({
-    required String key,
-  }) {
+  static dynamic readData({required String key}) {
     return box.read(key);
   }
 
-  static bool checkData({
-    required String key,
-  }) {
+  static bool checkData({required String key}) {
     return box.hasData(key);
   }
 
-  static void writeStringData({
-    required String key,
-    required String value,
-  }) {
+  static void writeStringData({required String key, required String value}) {
     box.write(key, value);
   }
 
-  static void writeBoolData({
-    required String key,
-    required bool value,
-  }) {
+  static void writeBoolData({required String key, required bool value}) {
     box.write(key, value);
   }
 
-  static void removeData({
-    required String key,
-  }) {
+  static void removeData({required String key}) {
     box.remove(key);
+  }
+
+  static void clearAllData() {
+    box.erase();
   }
 }
 
